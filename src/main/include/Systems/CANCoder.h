2409cb197163d132e28a365c8b3f2a47a793f4cb
NathanCorrea24 <correanathanh@gmail.com>
2022-09-18 18:31:09 -0700

    Added documentation to CANCoder and NeoMotor

--- src/main/include/Systems/CANCoder.h
+++ src/main/include/Systems/CANCoder.h
@@ -8,10 +8,26 @@ namespace FRC7110 {
   
     public:
       
+      /**
+       * @brief Create a CANCoder Object
+       * 
+       * @param ID CANId 
+       * @param offset Offset for CANCoder
+       */
       explicit CANcode(const int id, const double offset);
 
+      /**
+       * @brief Should be removed as soon as possible
+       * 
+       * @param DPP Distance Per Pulse
+       */
       void SetDistancePerPulse(const double DPP) const;
 
+      /**
+       * @brief Returns the position of the CANCoder in radians
+       * 
+       * @return The position of the CANCoder in Radians
+       */
       double Get();
     
     private:
--- src/main/include/Systems/NeoMotor.h
+++ src/main/include/Systems/NeoMotor.h
@@ -7,11 +7,28 @@ namespace FRC7110 {
   class NeoMotor : public rev::CANSparkMax, public rev::SparkMaxRelativeEncoder {
 
     public:
-
+       /**
+        * @brief Create a neo motor and use its encoder 
+        * 
+        * @param ID Motor ID
+        * @param Type Motor Type
+        */
       explicit NeoMotor(const int ID, rev::CANSparkMax::MotorType Type);
       
+      /**
+       * @brief This function is not neccessary and should 
+       * be removed from the project as soon as possible
+       * 
+       * 
+       * @param DPP distance per pulse
+       */
       void SetDistancePerPulse(double DPP) const;
       
+      /**
+       * @brief Get the rate of the motor adjusted for the MK4I
+       * 
+       * @return Rate of the Motor in MPS
+       */
       double GetRate();
 
   };
