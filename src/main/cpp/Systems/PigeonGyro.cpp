7d51e5a4fda8c235c69efb9c3f041f5e953dcf36
NathanCorrea24 <correanathanh@gmail.com>
2022-09-26 16:59:18 -0700

    Added documentation for new pigeon gyro class

--- src/main/cpp/Systems/PigeonGyro.cpp
+++ src/main/cpp/Systems/PigeonGyro.cpp
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #include "Systems/PigeonGyro.h"
 
 using namespace ctre::phoenix::sensors;
--- src/main/include/Systems/PigeonGyro.h
+++ src/main/include/Systems/PigeonGyro.h
@@ -7,14 +7,37 @@ namespace FRC7110 {
 
   class pigeonGyro : public frc::Gyro {
   public:
+
+    /**
+     * @brief Creates a new pigeon gyro object using CAN 
+     * 
+     * @param ID the CAN ID
+     */
     explicit pigeonGyro(int ID);
 
+    /**
+     * @brief Gets the angle the pigeon gyro is reading
+     * 
+     * @return angle in degrees 
+     */
     double GetAngle() const override;
 
+    /**
+     * @brief Gets the rate of rotation in degrees per second
+     * 
+     * @return double rate of rotation 
+     */
     double GetRate() const override;
 
+    /**
+     * @brief Resets the pigeon gyro heading
+     */
     void Reset() override;
 
+    /**
+     * @brief satisfies the pure virtual Calibrate() function found in
+     * frc::Gyro
+     */
     void Calibrate() override;
 
   private:
